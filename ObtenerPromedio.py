2dd1763fa897c148f35a195c1c4f785e44977959
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-07-11 22:30:38 -0500

    ejercicio en clases

---
+++ ObtenerPromedio.py
@@ -0,0 +1,11 @@
+# Elaborar un algoritmo que
+# Solicite 2 números y muestre
+# el promedio de ambos
+
+N1 = int(input("ingrese el número 1: "))
+N2 = int(input("ingrese el número 2: "))
+
+S = N1 + N2
+P = S / 2
+
+print(P)
